67b589bd24ed08d168a10d25a8eba073f729a63a
Benjamin Charlton <kuldrgames@gmail.com>
2018-08-24 17:50:35 +0100

    Added main functionality for now :) 1.0

--- sphealBot.py
+++ sphealBot.py
@@ -7,6 +7,7 @@ POST_LIMIT = 50
 
 clientTumblr = TUMBLR # Sets tokens for Tumblr stored in login
 clientDiscord = discord.Client()
+# Look into default discord help command
 
 clientTumblr.info() # Grabs the current user information
 
@@ -59,12 +60,19 @@ async def on_message(message):
     if clientDiscord.user.id != message.author.id:
         # If the bot is being issued a command checks to see if its in the correct channel
         #   This will hopefully avoid spam
-        if message.content.upper().startswith('!RNDSPHEAL') or message.content.upper().startswith('?RNDSPHEAL'):
+        if message.content.upper().startswith('SPHEAL!RND') or message.content.upper().startswith('SPHEAL?RND'):
             tmp = await clientDiscord.send_message(message.channel, 'Finding you a random spheal (:3)\"')
-            # Do stuff here
-
-        else:
-            await clientDiscord.send_message(message.channel, 'I\'m sorry I don\'t understand (:3)\"')
+            sphealURLArray = getAllSphealImagesURL()
+            await clientDiscord.edit_message(tmp, "I found all my friends, picking the cutest one for you (:3)\"")
+            rndURL = random.choice(sphealURLArray)
+            await clientDiscord.edit_message(tmp, "%s (:3)\"" % rndURL)
+        elif message.content.upper().startswith('SPHEAL!HELP') or message.content.upper().startswith('SPHEAL?HELP'):
+            helpText = ('Hiya (:3)\"\n'
+                        'Here are all the cool things I can do:\n\n'
+                        '- spheal!rnd will get you a random picture of my friends\n')
+            await clientDiscord.send_message(message.channel, '%s' % helpText)
+        elif message.content.upper().startswith('SPHEAL!') or message.content.upper().startswith('SPHEAL?'):
+            await clientDiscord.send_message(message.channel, 'I\'m sorry I don\'t understand (:3)\"\nYou can use spheal!help to see what I can do')
 
 # Run the bot with the token provided
 clientDiscord.run(TOKEN)
